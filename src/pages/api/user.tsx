61790635ae4ed1110a43e89b389a4028ec86768f
Antonio Bernardino da Silva <tonybsilvadev@gmail.com>
2021-01-25 23:43:48 -0300

    UpdateByName add

--- src/pages/api/user.tsx
+++ src/pages/api/user.tsx
@@ -1,6 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import express, { Request } from 'express';
 import moment from 'moment';
 import connect from '../../utils/database';
+import { Db } from 'mongodb';
+
+
+
 
 interface ErrorResponseType {
   error: string
@@ -8,16 +13,23 @@ interface ErrorResponseType {
 
 const today = moment().format('D MMMM  YYYY, h:mm:ss a')
 
-export default async (req: NextApiRequest, res: NextApiResponse<ErrorResponseType>) => {
+export default async (req: NextApiRequest & Request, res: NextApiResponse<ErrorResponseType>) => {
+
 
   //CRIAR UM USUÁRIO
   if (req.method === 'POST') {
-    const { name, email, description, phone, cpf, value, address, updated_at } = req.body;
-    if (!name || !value){
-      res.status(400).json({ error: 'Info missing' })
+
+    //Define oque é necessário para criar
+    const { name, email, description, phone, cpf, value, address } = req.body;
+
+    //Condicional para prosseguir
+    if (!name || !value) {
+      res.status(400).json({ error: 'Info is missing!' })
       return
     }
+    //Abre conexão com o banco (caso não esteja aberta)
     const { db } = await connect();
+    //Insere os dados
     const response = await db.collection('users').insertOne({
       name,
       email,
@@ -26,7 +38,8 @@ export default async (req: NextApiRequest, res: NextApiResponse<ErrorResponseTyp
       cpf,
       value,
       address,
-      created_at: today
+      created_at: today,
+      updated_at: today
     })
     console.log(response.ops[0])
     res.status(200).json(response.ops[0])
@@ -36,16 +49,60 @@ export default async (req: NextApiRequest, res: NextApiResponse<ErrorResponseTyp
   // }
 
   //BUSCAR UM USUÁRIO
-  if (req.method === 'GET'){
-    const { name, cpf } = req.body
-    if (!name || !cpf){
-      res.status(400).json({ error: 'Info missing' })
+  if (req.method === 'GET') {
+    //Necessário para buscar um usuáro
+    const { name } = req.body
+
+    //Condicional para prosseguir
+    if (!name) {
+      res.status(400).json({ error: 'Info is missing!' })
       return
     }
+    //Abre conexão com o Banco de dados
     const { db } = await connect();
+    //Realiza a pesquisa
     const response = await db.collection('users').findOne({ name: name })
+
     console.log(response)
     res.status(200).json(response)
+  }
+
+  if (req.method === "PUT") {
+
+    const { name } = req.query
+    const update = req.body
+    if (!name) {
+      res.status(400).json({ error: 'Name is missing!' })
+      return;
+    }
+
+    //Conexão com o banco de dados
+    const { db } = await connect();
+
+
+    //VALIDAÇÃO SE EXISTE OU NÃO NO BANCO PARA PODER SER ALTERADO
+    // const existsName = db.collection('users').find({"name": { $exists: true }})
+
+    // if (!existsName){
+    //   res.status(400).json({ error: 'Client not found!' })
+    //   return;
+    // }
+
+
+
+    // Procura o usuário e os atualiza.
+    const response = await db.collection('users').findOneAndUpdate({ "name": name }, { $set: update }, { returnOriginal: false })
+      .then(updatedDocument => {
+        if (updatedDocument) {
+          console.log(`Document is updated!, ${updatedDocument.value as any} `)
+          res.status(200).json(updatedDocument.value as any)
+        } else {
+          console.log(response)
+          res.status(400).json({ error: 'This user could not be updated!' })
+          return;
+        }
+      })
+
   }
 
 }
